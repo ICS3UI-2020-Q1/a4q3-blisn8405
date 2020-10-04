6c35a05f05accd537fef0c1648ff075a3a447a30
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 15:14:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 /**
  *
  * @author 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+   // create a scanner for user input
+Scanner input = new Scanner(System.in); 
+//create variable for user input
+   System.out.println("Enter a integer");
+   int number = input.nextInt();
+     //create a variable to keep track of what number we are on
+     int count = 1;
+        // the accumulator to add the count into
+     int total = 0;
+     while(count <= number){
+     total= total + count;
+     count= count + 1;
+     }
+     //print the total to the screen
+   System.out.println("The total is " + total);
   }
 }
